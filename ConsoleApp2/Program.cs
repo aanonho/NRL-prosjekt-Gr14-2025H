6dab7f0ecd5e546db64219ae688aaaaf9dcd9ad0
taikiyakitaiyaki <trinhmh@uia.no>
2025-08-19 13:49:06 +0200

    prøver noe nytt

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello gruppe 14!");
-// This is a comment
-// This is another comment
-// This is a third comment
-// This is a fourth comment
+string frukt = "banan";
+Console.WriteLine("Hej världen! " + frukt);
+// This is a comment in the code
