c135c0d5e9fa07f899259d2fa86529fa2814381c
taikiyakitaiyaki <trinhmh@uia.no>
2025-08-19 12:56:29 +0200

    test nr 1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 10;
+int y = 20;
+int sum = x + y;
+Console.WriteLine($"The sum of {x} and {y} is {sum}.");
