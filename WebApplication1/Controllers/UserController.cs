dba24aaaf57df39a64b842a75607b75e03465d75
taikiyakitaiyaki <trinhmh@uia.no>
2025-10-01 19:14:49 +0200

    oppdatert userStory1 med Tailwind og MVC

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult UserForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserForm(UserData userData)
+        {
+            return View("UserRegistrationOverview", userData);
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+
+    }
+}
