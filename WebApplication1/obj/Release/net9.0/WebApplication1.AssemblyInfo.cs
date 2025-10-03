0f92e7450214f81fd5d3db8abc13bb6a19b5ac95
taikiyakitaiyaki <trinhmh@uia.no>
2025-10-03 10:52:47 +0200

    Fjern genererte filer som skal ignoreres

--- WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba24aaaf57df39a64b842a75607b75e03465d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
