b12f0e87a643bc4fcd682f35f0513842f91943d8
MarieHesseberg <marie.hesseberg@hotmail.com>
2025-11-06 18:36:27 +0100

    created a registrar entity

---
+++ WebApplication1/Models/Entities/Registrar.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models.Entities
+{
+    [Table("Registrar")]
+    public class Registrar
+    {
+        // PK = FK to UserEntity per diagram
+        [Key, ForeignKey(nameof(User))]
+        [Column("UserData_UserID")]
+        public int UserID { get; set; }
+
+        [StringLength(45)]
+        public string? Department { get; set; }
+
+        public UserEntity User { get; set; } = null!;
+    }
+}
