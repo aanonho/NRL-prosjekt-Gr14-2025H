dc080992d52bf4a5899c0c3fffce234f64cc3c11
MarieHesseberg <marie.hesseberg@hotmail.com>
2025-11-06 18:35:07 +0100

    Created Pilot entity

---
+++ WebApplication1/Models/Entities/Pilot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models.Entities
+{
+    [Table("Pilot")]
+    public class Pilot
+    {
+        // PK = FK to UserEntity per diagram
+        [Key, ForeignKey(nameof(User))]
+        [Column("UserData_UserID")]
+        public int UserID { get; set; }
+
+        [StringLength(45)]
+        public string? ULicenseNumber { get; set; }
+
+        [StringLength(45)]
+        public string? AircraftType { get; set; }
+
+        public UserEntity User { get; set; } = null!;
+
+        // Reports written by this pilot
+        public ICollection<ReportItem> Reports { get; set; } = new List<ReportItem>();
+    }
+}
