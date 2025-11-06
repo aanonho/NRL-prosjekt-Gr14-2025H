f797baf1855f4e4d96184d7ab19d94a1fb014f60
MarieHesseberg <marie.hesseberg@hotmail.com>
2025-11-06 18:25:15 +0100

    Created Entities folder, moved ObstacleData and ReportItem

--- WebApplication1/Models/Entities/ReportItem.cs
+++ WebApplication1/Models/Entities/ReportItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApplication1.Models.Entities;
 
 namespace WebApplication1.Models
 {
--- WebApplication1/Models/ValidatedObstacleData.cs
+++ WebApplication1/Models/ValidatedObstacleData.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApplication1.Models.Entities;
 
 namespace WebApplication1.Models
 {
