edf99d3ad94f0683df470289575a142d8cb4bab6
taikiyakitaiyaki <trinhmh@uia.no>
2025-11-18 22:36:58 +0100

    endring på div utseende relaterte ting på nettsiden

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,13 +18,8 @@ namespace WebApplication1.Controllers
         }         
         public IActionResult Index()
         {       
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {          
-            return View();
-        }
+            return RedirectToAction("Login", "Account");
+        }   
     
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() // Error handling action
