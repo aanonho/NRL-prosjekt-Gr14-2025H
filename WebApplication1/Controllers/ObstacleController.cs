d6fb66164ab0b2dddc306289fc2b35934c6a7e96
taikiyakitaiyaki <trinhmh@uia.no>
2025-11-26 19:56:10 +0100

    fjernet feilmeldingen Rania fikk under brukertest + la til egendefinerte meldinger ut ifra report status

--- WebApplication1/Controllers/ObstacleController.cs
+++ WebApplication1/Controllers/ObstacleController.cs
@@ -63,8 +63,11 @@ namespace WebApplication1.Controllers
             }
             if (!report.IsDraft && string.Equals(report.Status, "Approved", StringComparison.OrdinalIgnoreCase))
             {
-                TempData["ErrorMessage"] = "Approved reports cannot be edited.";
-                return RedirectToAction("Index", "Reports");
+                ViewBag.IsEditing = false;
+                ViewBag.ReadOnly = true;
+                return View(report.ReportObstacle);
+                //TempData["ErrorMessage"] = "Approved reports cannot be edited."; -> Gammel kode som ikke lar piloter åpne apporoved rapporter for visning
+                //return RedirectToAction("Index", "Reports");
             }
 
             var normalizedEmail = email.Trim().ToLowerInvariant();
